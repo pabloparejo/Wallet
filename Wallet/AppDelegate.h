3719744ee5c42486506f1393ffdf0568b0b8a633
Pablo Parejo <parejo@Pablos-MacBook-Pro.local>
2015-03-13 18:17:12 +0100

    Initial Commit

---
+++ Wallet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Wallet
+//
+//  Created by Pablo Parejo Camacho on 13/3/15.
+//  Copyright (c) 2015 Pablo Parejo Camacho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
