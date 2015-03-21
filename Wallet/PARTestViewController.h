316d35d7c587a46344930fac29e6cca2c5aef2c2
Pablo Parejo <parejo@Pablos-MacBook-Pro.local>
2015-03-21 12:32:01 +0100

    Adding PARTestViewController

---
+++ Wallet/PARTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  PARTestViewController.h
+//  Wallet
+//
+//  Created by Pablo Parejo Camacho on 21/3/15.
+//  Copyright (c) 2015 Pablo Parejo Camacho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PARTestViewController : UIViewController
+
+@end
