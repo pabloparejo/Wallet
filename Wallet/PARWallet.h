49cf5aed6511da6cf1286210501566b98616ce60
Pablo Parejo <parejo@Pablos-MacBook-Pro.local>
2015-03-21 12:26:47 +0100

    Adding PARWallet

---
+++ Wallet/PARWallet.h
@@ -0,0 +1,13 @@
+//
+//  PARWallet.h
+//  Wallet
+//
+//  Created by Pablo Parejo Camacho on 21/3/15.
+//  Copyright (c) 2015 Pablo Parejo Camacho. All rights reserved.
+//
+
+#import "PARMoney.h"
+
+@interface PARWallet : PARMoney
+
+@end
---
+++ Wallet/PARWallet.m
@@ -0,0 +1,13 @@
+//
+//  PARWallet.m
+//  Wallet
+//
+//  Created by Pablo Parejo Camacho on 21/3/15.
+//  Copyright (c) 2015 Pablo Parejo Camacho. All rights reserved.
+//
+
+#import "PARWallet.h"
+
+@implementation PARWallet
+
+@end
